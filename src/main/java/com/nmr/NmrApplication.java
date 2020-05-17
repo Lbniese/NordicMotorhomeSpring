de50d0cf16b096a69f52df702caab71e4af4bad7
Lbniese <lbniese@gmail.com>
2020-05-17 12:30:27 +0200

    initial commit

---
+++ src/main/java/com/nmr/NmrApplication.java
@@ -0,0 +1,13 @@
+package com.nmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NmrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NmrApplication.class, args);
+    }
+
+}
