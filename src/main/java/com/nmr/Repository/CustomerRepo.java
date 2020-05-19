05073ce0da563a0a60a3843ad9e0b49b01490792
melander2910 <58288414+melander2910@users.noreply.github.com>
2020-05-19 11:19:52 +0200

    service, repo og zipcodesSQL

--- src/main/java/com/nmr/Repository/CustomerRepo.java
+++ src/main/java/com/nmr/Repository/CustomerRepo.java
@@ -1,4 +1,14 @@
 package com.nmr.Repository;
 
+import com.nmr.Model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class CustomerRepo {
+    @Autowired
+    JdbcTemplate template;
+
+    public Customer createCustomer(Customer customer) {
+        return null;
+    }
 }
