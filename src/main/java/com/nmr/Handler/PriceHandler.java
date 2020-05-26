5a7414f7f156875c4a1f5942dfe67887195b8a91
melander2910 <58288414+melander2910@users.noreply.github.com>
2020-05-26 11:59:00 +0200

    Season check in pricehandler

--- src/main/java/com/nmr/Handler/PriceHandler.java
+++ src/main/java/com/nmr/Handler/PriceHandler.java
@@ -3,34 +3,52 @@ package com.nmr.Handler;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import static java.time.temporal.ChronoUnit.DAYS;
 
 public class PriceHandler {
 
     public static double calculateFullPrice(LocalDateTime rentalStartDate, LocalDateTime rentalEndDate, int pricePerDay){
-        long days = DAYS.between(rentalStartDate, rentalEndDate);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-
-        LocalDateTime peakSeasonStartDate = LocalDateTime.parse("2020-06-01 00:00", formatter);
-        LocalDateTime peakSeasonEndDate = LocalDateTime.parse("2020-08-31 00:00",formatter);
-
-        LocalDateTime midSeasonStartDate = LocalDateTime.parse("2021-03-01 00:00", formatter);
-        LocalDateTime midSeasonEndDate = LocalDateTime.parse("2021-05-31 00:00", formatter);
-
-        LocalDateTime lowSeasonStartDate = LocalDateTime.parse("2020-09-01 00:00", formatter);
-        LocalDateTime lowSeasonEndDate = LocalDateTime.parse("2021-02-29 00:00", formatter);
 
         double fullPrice = 0;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        LocalDateTime peakSeasonStartDate = LocalDateTime.parse("2020-06-01 00:00", formatter);
+        LocalDateTime peakSeasonEndDate = LocalDateTime.parse("2020-08-31 23:59", formatter);
 
-        if(!rentalStartDate.isBefore(peakSeasonStartDate) && !rentalEndDate.isAfter(peakSeasonEndDate)){
-            fullPrice = days * (pricePerDay * 1.6);
-        }
-        if(!rentalStartDate.isBefore(midSeasonStartDate) && !rentalEndDate.isAfter(midSeasonEndDate)){
-            fullPrice = days * (pricePerDay * 1.3);
-        }
-        if(!rentalStartDate.isBefore(lowSeasonStartDate) && !rentalEndDate.isAfter(lowSeasonEndDate)){
-            fullPrice = days * (pricePerDay * 1.0);
+        LocalDateTime midSeasonStartDate = LocalDateTime.parse("2020-03-01 00:00", formatter);
+        LocalDateTime midSeasonEndDate = LocalDateTime.parse("2020-05-31 23:59", formatter);
+        
+        LocalDateTime currentDate = rentalStartDate;
+        boolean addPrice = false;
+
+        while(currentDate.isBefore(rentalEndDate) || currentDate.isEqual(rentalEndDate)){
+            if(currentDate.getMonthValue() >= peakSeasonStartDate.getMonthValue() && currentDate.getMonthValue() <= peakSeasonEndDate.getMonthValue()){
+                addPrice = true;
+                if(currentDate.getMonthValue() == peakSeasonStartDate.getMonthValue() && currentDate.getDayOfMonth() < peakSeasonStartDate.getDayOfMonth()){
+                    addPrice = false;
+                }
+                if(currentDate.getMonthValue() == peakSeasonEndDate.getMonthValue() && currentDate.getDayOfMonth() > peakSeasonEndDate.getDayOfMonth()){
+                    addPrice = false;
+                }
+                if(addPrice){
+                    fullPrice += pricePerDay * 1.6;
+                }
+            }
+            if(currentDate.getMonthValue() >= midSeasonStartDate.getMonthValue() && currentDate.getMonthValue() <= midSeasonEndDate.getMonthValue()){
+                addPrice = true;
+                if(currentDate.getMonthValue() == midSeasonStartDate.getMonthValue() && currentDate.getDayOfMonth() < midSeasonStartDate.getDayOfMonth()){
+                    addPrice = false;
+                }
+                if(currentDate.getMonthValue() == midSeasonEndDate.getMonthValue() && currentDate.getDayOfMonth() > midSeasonEndDate.getDayOfMonth()){
+                    addPrice = false;
+                }
+                if(addPrice){
+                    fullPrice += pricePerDay * 1.3;
+                }
+            }
+            if(!addPrice){
+                fullPrice += pricePerDay * 1.0;
+            }
+            currentDate = currentDate.plusDays(1);
+            addPrice = false;
         }
         return fullPrice;
     }
