bea582f96fe53f659b2eeccc4f8949ad3af1da94
Lbniese <Lbniese@users.noreply.github.com>
2020-05-30 18:21:26 +0200

    Improved error handling
    *Improved error handling
    *Improved error 500 page
    *Added customererrorcontroller documentation

--- src/main/java/com/nmr/Controller/CustomErrorController.java
+++ src/main/java/com/nmr/Controller/CustomErrorController.java
@@ -3,6 +3,7 @@ package com.nmr.Controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.RequestDispatcher;
@@ -12,14 +13,21 @@ import javax.servlet.http.HttpServletRequest;
 public class CustomErrorController implements ErrorController {
 
     /**
-     * handleError() display specific error pages for different error types.
-     *
+     * handleError() displays specific error pages for different error types.
+     * handleError() models the exception as errorMessage to display on the error page.
      * @param request
      * @return
      */
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
+    public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        model.addAttribute("errorCode", request.getAttribute("javax.servlet.error.status.code"));
+        Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+        String errorMessage = null;
+        if (throwable != null) {
+            errorMessage = throwable.getMessage();
+        }
+        model.addAttribute("errorMessage", errorMessage);
 
         if (status != null) {
             Integer statusCode = Integer.valueOf(status.toString());
@@ -35,7 +43,7 @@ public class CustomErrorController implements ErrorController {
     }
 
     /**
-     * getErrorPath overrides the implemented ErrorController's method to return a custom path to cal lwhen an error occured.
+     * getErrorPath overrides the implemented ErrorController's method to return a custom path to call when an error occured.
      * @return
      */
     @Override
